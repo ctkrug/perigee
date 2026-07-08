c09fbceb9fd2bb38a752d1a177531d8577564acc
ctkrug <ctkrug4501@gmail.com>
2026-07-08 10:25:46 +0000

    build: configure eslint flat config with browser and test globals

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        localStorage: "readonly",
+        requestAnimationFrame: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    files: ["test/**/*.js"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+      },
+    },
+  },
+];
